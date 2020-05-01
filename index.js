f8d1c163e3e6ee4874f7b39afa997f384947fea2
sofia Luna <Sofia21luna@gmail.com>
2020-04-30 22:15:52 -0400

    class car works!

--- index.js
+++ index.js
@@ -77,8 +77,30 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
 
-}
+  fill(gallons){
+    this.tank += gallons;
+  }
+
+  drive(distance){
+    let useGas = (distance / this.milesPerGallon);
+    if (useGas > this.tank) {
+      this.odometer += this.tank * this.milesPerGallon;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    } else {
+      this.odometer += distance;
+      this.tank -= useGas;
+    }
+  }
+  }
+
 
 /*
   TASK 3
